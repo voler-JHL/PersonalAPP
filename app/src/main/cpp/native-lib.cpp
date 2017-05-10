3e816fe77abe9d004a3a8daf1c06a75bcddcdcc2
voler-HJL <1006873533@qq.com>
2017-05-10 19:15:01 +0800

    创建新项目

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,12 @@
+
+#include <jni.h>
+#include <string>
+
+extern "C"
+jstring
+Java_com_voler_person_personalapp_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
---
+++ app/src/main/java/com/voler/person/processor/XProcessor.java
@@ -0,0 +1,12 @@
+package com.voler.person.processor;
+
+import java.util.ArrayList;
+
+/**
+ * XProcessor Created by voler on 2017/5/9.
+ * 说明：
+ */
+
+
+public class XProcessor extends ArrayList{
+}
